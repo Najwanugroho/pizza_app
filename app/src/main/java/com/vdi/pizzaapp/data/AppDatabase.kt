8e2f88e5ce759921bb9dec50d17283bc06be539c
Najwa Shaqila Isna Nugroho <email@gmail.com>
2025-08-20 16:24:23 +0700

    enkripsi database

--- app/src/main/java/com/vdi/pizzaapp/data/AppDatabase.kt
+++ app/src/main/java/com/vdi/pizzaapp/data/AppDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 
 @Database(entities = [Pizza::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
@@ -12,13 +14,30 @@ abstract class AppDatabase : RoomDatabase() {
     companion object {
         @Volatile private var INSTANCE: AppDatabase? = null
 
+
         fun getInstance(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
-                Room.databaseBuilder(
+
+                //non encrypted
+            /*    Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "pizza_db"
                 ).build().also { INSTANCE = it }
+*/
+
+                //encrypted
+                SQLiteDatabase.loadLibs(context)
+                val  passphrase:ByteArray= SQLiteDatabase.getBytes("my_secure_key".toCharArray())
+                val  factory = SupportFactory(passphrase)
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "encrypted-database"
+                )
+                    .openHelperFactory(factory)
+                    .build()
+                    .also { INSTANCE=it }
             }
         }
     }
